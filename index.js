43c93cc16b82c6522c38ff2329785b09964028f9
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-12 21:10:10 +0530

    fcommit

---
+++ index.js
@@ -0,0 +1,28 @@
+var music = /** @class */ (function () {
+    function music(Config) {
+        this.genre = Config.genre;
+    }
+    return music;
+}());
+var love = new music({
+    genre: ["https://giphy.com/stories/confused-pups-0bf9b19d-789a",
+        "music\Ennodu Nee Irundhal.mp3",
+        "music\Maruvaarthai Pesathey.mp3",
+        "MUSIC PLAYER\music\Kannama.mp3"
+    ]
+});
+var t19 = new music({
+    genre: [
+        "music\Ellu Vaya Pookalaye.mp3",
+        "music\Adiye.mp3",
+        "music\Yen Minukki.mp3",
+        "music\Maruvaarthai Pesathey.mp3"
+    ]
+});
+var t20 = new music({
+    genre: ["music\Kolame.mp3",
+        "music\Kutti Story.mp3",
+        "music\Vaathi Coming.mp3",
+        "music\Veyyon Silli.mp3",
+    ]
+});
---
+++ index.ts
@@ -0,0 +1,36 @@
+interface Config {
+    genre: any[];
+}
+class songs {
+    url1:["https://media.giphy.com/media/3HAYjfk9hasigrDVJRK/giphy.gif",
+    "https://media.giphy.com/media/gIfv29q3ULtqjYTR7B/giphy.gif",
+    "https://media.giphy.com/media/CPskAi4C6WLHa/giphy.gif",
+    "https://media.giphy.com/media/kEdBEtNgSD36ufdgJr/giphy.gif"
+];
+    url2:["https://media.giphy.com/media/3oEjHZPivwdJ0syhKE/giphy.gif",
+    "https://media.giphy.com/media/xUOwFZon2EeYABAPmM/giphy.gif",
+    "https://media.giphy.com/media/l0HeoePKZ841bZzby/giphy.gif",
+    "https://media.giphy.com/media/13ByqbM0hgfN7y/giphy.gif"
+];
+    url3:["https://media.giphy.com/media/nKVj8Y39vAshaZaCd9/giphy.gif",
+    "https://media.giphy.com/media/YmVNzDnboB0RQEpmLr/giphy.gif",
+    "https://media.giphy.com/media/26gsnlYjswkyY3ENq/giphy.gif",
+    "https://media.giphy.com/media/J6OrTexOCzH9XcF8Vg/giphy.gif"
+];
+    url4:["https://media.giphy.com/media/3HSzHiDUtNLhu/giphy.gif",
+    "https://media.giphy.com/media/shJoaSf7VpGnu/giphy.gif",
+    "https://media.giphy.com/media/d8YBM0IgPszle/giphy.gif",
+    "https://media.giphy.com/media/fgR5ghGSXWpuU/giphy.gif"
+]}
+
+let AddButton = <HTMLButtonElement>document.getElementById('ADD')
+AddButton.addEventListener('click', () => {
+  var genre = <HTMLInputElement>document.getElementById("genre");
+  var url = <HTMLInputElement>document.getElementById("url");
+
+});
+
+    
+
+
+
